ef83d132ff28f99570b1833a341581082c1c0248
dmipet90 <dmitri.petrussevits@gmail.com>
2020-01-17 00:21:48 +0200

    React front end added

---
+++ src/main/resources/static/precache-manifest.a99817ad9e984ea2a2485acec8607e12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e773e4b8cdb0fc6b61a72531731fd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c5adf09dd4eba2a7640",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ef8c041bf67c409fe979",
+    "url": "/static/css/main.3d9f99c1.chunk.css"
+  },
+  {
+    "revision": "6c5adf09dd4eba2a7640",
+    "url": "/static/js/2.48858248.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.48858248.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef8c041bf67c409fe979",
+    "url": "/static/js/main.cbe922a0.chunk.js"
+  },
+  {
+    "revision": "676fb1584492815bdf2f",
+    "url": "/static/js/runtime-main.fd710286.js"
+  }
+]);
